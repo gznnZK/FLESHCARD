06b1ba8082704a43aaea07344bce7641bebafb60
gznnZK <164032660+gznnZK@users.noreply.github.com>
2024-11-12 13:34:03 +0000

    rfrtrgt

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'geral',
+    'qual é a melhor profesora do Julia Cavassin?',
+    'a professora Itakinha'
+)
+
+criaCartao(
+    'conhecimento',
+    'qual professora merece um aumento?',
+    'A prof cristina de programação'
+)
+
+criaCartao(
+    'Aleatoria',
+    'qual prof voce mais ama?',
+    'a itakinha'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em ingles?',
+    'I love prof cristina'
+)
